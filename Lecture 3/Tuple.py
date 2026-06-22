a172f38c4489ac37ab9dcdf361a9dd9fedf80c01
krishbhut95-design <krishbhut95@gmail.com>
2026-06-22 17:22:46 +0530

    Tuple

---
+++ Lecture 3/Tuple.py
@@ -0,0 +1,24 @@
+tup = (1 , 2, 3) #Tuple
+print(tup)
+print(type(tup))
+
+tup = (1)        #Intger
+print(tup)
+print(type(tup))
+
+
+tup = ("Hello")   #String
+print(tup)
+print(type(tup))
+
+
+#Slicing
+tup = (1, 4, 5, 7)        
+print(tup[1:3])
+
+
+#index & Count
+tup = (1, 4, 6, 5, 7)        
+print(tup.index(6))
+print(tup.count(4))
+
